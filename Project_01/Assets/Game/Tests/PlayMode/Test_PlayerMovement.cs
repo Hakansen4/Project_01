8a359d7e0bdba42b6ff9015b8e8c4db15ec8896c
Ali Hakan Şen <alihakansen4@gmail.com>
2023-12-17 00:02:33 +0300

    feat(Player): Player Movement Test
    Player Movement tests are done.

---
+++ Project_01/Assets/Game/Tests/PlayMode/Test_PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Test_PlayerMovement
+{
+    [UnityTest]
+    public IEnumerator Test_PlayerRun()
+    {
+        GameObject gm = new GameObject();
+        gm.AddComponent<Rigidbody2D>();
+        float runningSpeed = 10.0f;
+        float jumpPower = 10.0f;
+        float pushSpeed = 10.0f;
+
+        PlayerMovement movement = new PlayerMovement(runningSpeed, gm.GetComponent<Rigidbody2D>(),
+            gm.GetComponent<Transform>(), jumpPower, pushSpeed);
+
+
+        movement.Run();
+
+        Assert.AreEqual(runningSpeed * Time.fixedDeltaTime * InputManager.GetHoriontalValue()
+                        , gm.GetComponent<Rigidbody2D>().velocity.x);
+
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator Test_PlayerPush()
+    {
+        GameObject gm = new GameObject();
+        gm.AddComponent<Rigidbody2D>();
+        float runningSpeed = 10.0f;
+        float jumpPower = 10.0f;
+        float pushSpeed = 10.0f;
+
+        PlayerMovement movement = new PlayerMovement(runningSpeed, gm.GetComponent<Rigidbody2D>(),
+            gm.GetComponent<Transform>(), jumpPower, pushSpeed);
+
+        movement.PushMove();
+
+        Assert.AreEqual(pushSpeed * Time.fixedDeltaTime * InputManager.GetHoriontalValue()
+                        , gm.GetComponent<Rigidbody2D>().velocity.x);
+
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator Test_PlayerJump() 
+    {
+        GameObject gm = new GameObject();
+        gm.AddComponent<Rigidbody2D>();
+        float runningSpeed = 10.0f;
+        float jumpPower = 10.0f;
+        float pushSpeed = 10.0f;
+
+        PlayerMovement movement = new PlayerMovement(runningSpeed, gm.GetComponent<Rigidbody2D>(),
+            gm.GetComponent<Transform>(), jumpPower, pushSpeed);
+
+        movement.Jump();
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreNotEqual(0.0f, gm.GetComponent<Rigidbody2D>().velocity.y);
+
+        yield return null;
+    }
+}
