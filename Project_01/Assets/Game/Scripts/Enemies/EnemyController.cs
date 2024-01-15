ae153b73aca53815ef2d8281691ec6259b166311
Ali Hakan Şen <alihakansen4@gmail.com>
2024-01-15 04:40:46 +0300

    feat(Enemy) New Enemy Type(Enemy_0)
    Standart enemy added. This is Enemy_0. Enemy_0 can chase, patrol and attack.

--- Project_01/Assets/Game/Scripts/Enemies/EnemyController.cs
+++ Project_01/Assets/Game/Scripts/Enemies/EnemyController.cs
@@ -16,29 +16,31 @@ public class EnemyController : MonoBehaviour
     private const string STATES = "STATES";
     #endregion
     #region States
-    [FoldoutGroup(STATES), SerializeField] private StateMachine _stateMachine;
-    [FoldoutGroup(STATES), SerializeField] private State _patrolState;
-    [FoldoutGroup(STATES), SerializeField] private State _chaseState;
-    [FoldoutGroup(STATES), SerializeField] private State _attackState;
-    [FoldoutGroup(STATES), SerializeField] private State _idleState;
+    [FoldoutGroup(STATES), SerializeField] protected StateMachine _stateMachine;
+    [FoldoutGroup(STATES), SerializeField] protected State _patrolState;
+    [FoldoutGroup(STATES), SerializeField] protected State _chaseState;
+    [FoldoutGroup(STATES), SerializeField] protected State _attackState;
+    [FoldoutGroup(STATES), SerializeField] protected State _idleState;
     #endregion
     #region Components
     [FoldoutGroup(COMPONENTS), SerializeField] private Rigidbody2D _rigidbody;
     [FoldoutGroup(COMPONENTS), SerializeField] private Animator _animator;
     [FoldoutGroup(COMPONENTS), SerializeField] private Transform _leftBorder;
     [FoldoutGroup(COMPONENTS), SerializeField] private Transform _rightBorder;
-    [FoldoutGroup(COMPONENTS), SerializeField] private Transform _player;
+
+    [FoldoutGroup(COMPONENTS), SerializeField] protected Transform _player;
 
     public EnemyMovement movement;
     #endregion
     #region Values
     [SerializeField] private float _speed;
-    [SerializeField] private float _chaseRange;
-    [SerializeField] private float _attackRange;
-    [SerializeField] private float _attackCooldown;
 
-    private float range = 0;
-    private float attackTimer = 0;
+    [SerializeField] protected float _chaseRange;
+    [SerializeField] protected float _attackRange;
+    [SerializeField] protected float _attackCooldown;
+
+    protected float range = 0;
+    protected float attackTimer = 0;
     #endregion
 
     private void Awake()
@@ -49,13 +51,16 @@ public class EnemyController : MonoBehaviour
     {
         CheckStateTransition();
     }
-    private void CheckStateTransition()
+    protected virtual void CheckStateTransition()
     {
         range = Mathf.Abs(_player.position.x - transform.position.x);
 
+        if (_stateMachine.CurrentState == _attackState)
+            return;
+
         if(range <= _attackRange)
         {
-            if(_stateMachine.CurrentState != _attackState   &&  Time.time - attackTimer >= _attackCooldown)
+            if(Time.time - attackTimer >= _attackCooldown)
             {
                 _stateMachine.TransitionTo(_attackState);
                 attackTimer = Time.time;
@@ -65,17 +70,17 @@ public class EnemyController : MonoBehaviour
                 _stateMachine.TransitionTo(_idleState);
             }
         }
-        else if (range <= _chaseRange && _stateMachine.CurrentState != _attackState)
+        else if (range <= _chaseRange)
         {
             if (_stateMachine.CurrentState != _chaseState)
                 _stateMachine.TransitionTo(_chaseState);
         }
-        else if(range > _chaseRange && _stateMachine.CurrentState != _attackState)
+        else if(range > _chaseRange)
         {
             if (_stateMachine.CurrentState != _patrolState)
                 _stateMachine.TransitionTo(_patrolState);
         }
-        else if(_stateMachine.CurrentState != _attackState)
+        else
         {
             if(_stateMachine.CurrentState != _idleState)
                 _stateMachine.TransitionTo(_idleState);
